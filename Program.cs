2ff8aadfc9b20cc4b575a9b12ce15bfcefc4718b
Yashwanth Reddy Bommineni <S531519@nwmissouri.edu>
2019-01-24 18:05:54 -0600

    updated the app with all requirements and functionalities

--- Program.cs
+++ Program.cs
@@ -6,103 +6,120 @@ namespace DotNet_firstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("********Domestic Parcel Service*******");
-            Console.WriteLine("Please enter your name:");
+            Console.WriteLine("********Domestic Parcel Cost Estimator*******"+"\n\n"+"Please enter your name:");
             string userName = Console.ReadLine();
-            Console.WriteLine("Welcome ,"+userName +"!");
-            Console.WriteLine("Enter the no. of parcels you want to include in the estimation");
+            Console.WriteLine("\n"+"Welcome "+userName+"!"+"\n\n"+"Please enter the no. of parcels you want to include in the estimation :");
             int numParcels =  Convert.ToInt32(Console.ReadLine());
             double Tcost = 0.0;
-            
+            bool like = false;
             for(int i=1;i<=numParcels;i++)
             {
-                Console.WriteLine("Choose the size of the box you want:");
-                Console.WriteLine("1. Small");
-                Console.WriteLine("2. Medium");
-                Console.WriteLine("3. Large");
-                int number = Convert.ToInt32(Console.ReadLine());
-                if(number==1)
+                Console.WriteLine("\n"+"Choose the size of the box you want for parcel number "+i+" :"+"\n");
+                Console.WriteLine("a. Small");
+                Console.WriteLine("b. Medium");
+                Console.WriteLine("c. Large");
+                char alpha = Convert.ToChar(Console.ReadLine());
+                if(alpha=='a')
                     Console.WriteLine("You have opted for a small box.");
-                else if(number==2)
+                else if(alpha=='b')
                     Console.WriteLine("You have opted for a medium box.");
-                else if(number==3)
+                else if(alpha=='c')
                     Console.WriteLine("You have opted for a large box.");
                 else
-                     Console.WriteLine("You have opted invalid entry");    
+                     Console.WriteLine("You have opted invalid entry.");    
 
-                Console.WriteLine("Please enter the weight of your parcel in lbs:");
+                Console.WriteLine("\n"+"Please enter the weight of your parcel in lbs:");
                 
                 double pWeight = 0.0;
                 pWeight = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Your given weight is "+ pWeight +" lbs." );
+                Console.WriteLine($"Your given weight is {pWeight} lbs." );
                double cost = 0.0;
                 
-                switch(number)
+                switch(alpha)
                 {
                     //double cost = 0;
-                    case 1 :
+                    case 'a' :
                         
                         if (pWeight <= 0)
                         {
-                            Console.WriteLine("You have entered invalid weight.");
+                           // Console.WriteLine("You have entered invalid weight.");
                         }
                         else if(pWeight >0 && pWeight<10)
                         {
-                            cost = pWeight * 1;
-                            Console.WriteLine("Total cost for your parcel service is $" +cost);
+                            cost = Cost(pWeight,1);
+                           // Console.WriteLine("Cost estimation for your parcel number "+i+" is $" +cost);
                         }
                         else
                         {
-                            cost = pWeight * 2;
-                        Console.WriteLine("Total cost for your parcel service is $" +cost);
+                            cost = Cost(pWeight,2);
+                       // Console.WriteLine("Cost estimation for your parcel number "+i+" is $" +cost);
                         }
                         break;
-                    case 2 :
+                    case 'b' :
                     //double cost=0;
                         if (pWeight <= 0)
                         {
-                            Console.WriteLine("You have entered invalid weight.");
+                           // Console.WriteLine("You have entered invalid weight.");
                         }
                         else if(pWeight >0 && pWeight<10)
                         {
-                            cost = pWeight * 1.5;
-                            Console.WriteLine("Total cost for your parcel service is $" +cost);
+                            cost = Cost(pWeight,1.5);
+                           // Console.WriteLine("Cost estimation for your parcel number "+i+" is $" +cost);
                         }
                         else
                         {
-                            cost = pWeight * 2.5;
-                        Console.WriteLine("Total cost for your parcel service is $" +cost);
+                            cost = Cost(pWeight,2.5);
+                        //Console.WriteLine("Cost estimation for your parcel number "+i+" is $" +cost);
                         }
                         break;
-                    case 3 :
+                    case 'c' :
                     //double cost=0;
                         if (pWeight <= 0)
                         {
-                            Console.WriteLine("You have entered invalid weight.");
+                          //  Console.WriteLine("You have entered invalid weight.");
                         }
                         else if(pWeight >0 && pWeight<10)
                         {
-                            cost = pWeight * 1.75;
-                            Console.WriteLine("Total cost for your parcel service is $" +cost);
+                            cost = Cost(pWeight,1.75);
+                         //   Console.WriteLine("Cost estimation for your parcel number "+i+" is $" +cost);
                         }
                         else
                         {
-                            cost = pWeight * 2.75;
-                        Console.WriteLine("Total cost for your parcel service is $" +cost);
+                            cost = Cost(pWeight,2.75);
+                       // Console.WriteLine("Cost estimation for your parcel number "+i+" is $" +cost);
                         }
                         break;
 
                     default :
                         cost = 0.0;
-                        Console.WriteLine("Total cost for your parcel service is $ " + cost);
+                        //Console.WriteLine("Cost estimation for your parcel number "+i+" is $" +cost);
                         break;
                     
                 }
 
-            Console.WriteLine(" cost for parcel number "+ i + " is "+cost);
+           Console.WriteLine("Cost estimation for your parcel number "+i+" is $" +cost);
             Tcost = Tcost + cost ;
         }
-        Console.WriteLine("Total cost for all the parcels is " +Tcost);
+        decimal TotalCost = (decimal)(Tcost);
+
+        Console.WriteLine("\n"+"Total cost for all the parcels is $ " +TotalCost);
+        Console.WriteLine("\n"+"Did you like our app ? y/n");
+        
+        var option = Console.ReadLine();
+        if (option == "y") {
+            like = true;
+        }
+        if (like) {
+            Console.WriteLine("Thank you so much !");
+        } else {
+            Console.WriteLine("Thank you, Please leave your suggestion.");
+            string suggestion = Console.ReadLine();
+        }
+        
+
     } 
+    static double Cost(double pWeight, double number) {
+        return pWeight * number;
+    }
     }
 }
